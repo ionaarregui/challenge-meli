584b5da87f55bbf660581dd503f483fcdc6dfb63
Iona Arregui <iona.arregui@gmail.com>
2025-08-03 21:51:34 -0300

    Add breadcrumb action

--- client/src/components/BreadCrumb/BreadCrumb.tsx
+++ client/src/components/BreadCrumb/BreadCrumb.tsx
@@ -1,13 +1,24 @@
 import styles from './BreadCrumb.module.scss'
+import { useNavigate } from 'react-router-dom'
 interface PropsBreadCrumb {
   steps: string[]
 }
 
 export default function BreadCrumb({ steps }: PropsBreadCrumb) {
+  const navigate = useNavigate()
+
+  const handleClick = (step: string) => {
+    navigate(`/items?search=${step}`)
+  }
+
   return (
     <div className={styles.breadcrumb}>
       {steps.map((step, index) => (
-        <span className={styles['breadcrumb-step']} key={index}>
+        <span
+          className={styles['breadcrumb-step']}
+          key={index}
+          onClick={() => handleClick(step)}
+        >
           {step}
           {index < steps.length - 1 && ' > '}
         </span>
